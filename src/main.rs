b0fb2256cdc1977246d0f1c8f0dfb8f10a49fd60
Configurator <@inbox.ru>
2023-03-21 00:44:53 +0300

    saving

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 #![allow(non_snake_case)]
 
+use clap::Parser;
+
 use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, cli world!");
 
     for item in env::args() {
         println!("item: {}", item);
     }
+
+    println!("parser intro");
+
+    let args = FirstCLI::parse();
+    println!("{} -> {}", args.command, args. value);
 }
+
+
+#[derive(Parser)]
+struct FirstCLI {
+    command: String,
+    value: i64,
+}
+
